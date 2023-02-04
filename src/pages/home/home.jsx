e82e47850bbba3f5c293550d3411e4b2e89c7b87
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2023-02-04 15:39:32 +0300

    v7

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 import Videos from '../../components/Videos'
 
 const Home = () => {
+
+    const baseUrl ="https://googleapis.com/youtube/v3"
+
+    const getYoutubeVideos =async ()=>{
+        try {
+            await axios.get(`${baseUrl}/videos?part=snippetchart=mostPopular`, {
+                params:{
+                    key:process.env.REACT_APP_API_KEY,
+                    maxResults:50
+                }
+            } )
+        } catch (error) {
+            console.log(error.message)
+        }
+
+    }
+
+    useEffect(() => {
+
+      getYoutubeVideos()
+    }, [])
+    
+
+
   return (
     <div>
         <Videos/>
