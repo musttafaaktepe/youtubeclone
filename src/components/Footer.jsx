cb8d3b1c25776258c3fd458808b20f51457f72a9
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2023-02-04 18:26:22 +0300

    v11

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+    <footer className="flex flex-col items-center bottom-0 left-0 w-full bg-[#333333] text-[#FAFAFA] py-4 ">
+      <p>
+        © {new Date().getFullYear()} Copyright 
+      </p> 
+        <p>
+        Developed by {" "}
+        <a
+          target="_blank"
+          className="text-white underline"
+          href="https://github.com/codeDiaries-devTeam"
+        >
+          codeDiaries-devTeam
+        </a>
+      </p>
+    </footer>
+</div>
   )
 }
 
